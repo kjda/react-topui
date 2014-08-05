c6296425930159d795daea258d67c0db8e12b29f
kjda <khaled.jouda@ymail.com>
2014-08-06 01:41:57 +0200

    rem dist

--- lib/textarea.jsx
+++ lib/textarea.jsx
@@ -2,25 +2,26 @@
 var React = require('react/addons');
 module.exports = React.createClass({
 	
-	displayName: 'ButtonBar',
+	displayName: 'Textarea',
 
 	getDefaultProps: function(){
 		return {
+			large: false,
 			full: false
 		};
 	},
-	
+
+
 	render: function(){
-
+		
 		var classSet = React.addons.classSet({
-			'topcoat-button-bar': true,
-			'full': this.props.full
+			'topcoat-textarea': !this.props.large ,
+			'topcoat-textarea--larg': this.props.large,
+			full: this.props.full
 		});
-
+		
 		return this.transferPropsTo(
-			React.DOM.div( {className:classSet}, 
-			this.props.children
-			)
+			<textarea type="text" className={classSet} />
 		);
 	}
 });
