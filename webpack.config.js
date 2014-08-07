11c3dbd3d54e8d9c6bca4b0b530fdcd65991e7d1
kjda <khaled.jouda@ymail.com>
2014-08-07 19:39:32 +0200

    build

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,33 @@ var webpack = require('webpack');
 var UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
 
 module.exports = {
-    //target: 'web',
+    target: 'web',
     //debug: true,
     //devtool: 'source-map',
     context: __dirname + '/lib/',
     watch: true,
-    entry: __dirname + '/lib/index.jsx',
+    entry: [
+    __dirname + '/lib/index.jsx'    
+    ],
     output: {
         path: __dirname + '/dist/',
         filename: 'index.js',
-        library: 'ReactTopui',
+        library: 'TopUI',
         libraryTarget: 'umd'
     },
     resolve: {
-        extensions: ['', '.js', '.jsx'],
-       // modulesDirectories: [  '../node_modules'],
+        extensions: ['', '.js', '.jsx']
     },
     module: {
         loaders: [
-        //{ test: /\.css/, loader: 'style-loader!css-loader' },
-        //{ test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
         { test: /\.jsx$/, loader: '../node_modules/jsx-loader' }
         ]    
     },
     externals: {
         'react': 'React'
     },
+
     plugins: [
-        new UglifyJsPlugin()
+    new UglifyJsPlugin()
     ]
-    };
+};
